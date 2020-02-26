433604405029c0a8fda48fce180e64498ec650fb
Aleksei Semenov <aleksei.semenov@akvelon.com>
2020-02-26 16:49:11 +0300

    Updates

---
+++ precache-manifest.c0fd89ea23130b86df1939d99e02c09d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b70e0f904e5c4ca4272a342dedea2304",
+    "url": "/LunchMates/index.html"
+  },
+  {
+    "revision": "cf7b6cb547c05c3fbb50",
+    "url": "/LunchMates/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e1b1a20d4624ccde4b87",
+    "url": "/LunchMates/static/js/2.5649a717.chunk.js"
+  },
+  {
+    "revision": "ea8761bfdc77ba82ac0139b0e0ab83b5",
+    "url": "/LunchMates/static/js/2.5649a717.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf7b6cb547c05c3fbb50",
+    "url": "/LunchMates/static/js/main.eb170850.chunk.js"
+  },
+  {
+    "revision": "1879fa80719b39e3358f",
+    "url": "/LunchMates/static/js/runtime-main.38801f1f.js"
+  }
+]);
